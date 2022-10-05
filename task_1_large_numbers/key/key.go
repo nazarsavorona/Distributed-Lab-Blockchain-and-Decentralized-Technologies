a3fe1e1d702d0a4f4ca5138cc6b9fef6f820d4c5
Nazarii Savorona <nazarii_savorona@epam.com>
2022-10-05 14:07:27 +0300

    Add key generation

--- task_1_large_numbers/key/key.go
+++ task_1_large_numbers/key/key.go
@@ -1,6 +1,7 @@
 package key
 
 import (
+	"crypto/rand"
 	"math/big"
 )
 
@@ -9,4 +10,17 @@ func keySpace(bitsCount int64) *big.Int {
 	two := big.NewInt(2)
 
 	return two.Exp(two, bits, nil)
+}
+
+func generateKey(bitsCount int64) (*big.Int, error) {
+	max := new(big.Int)
+	max = keySpace(bitsCount).Sub(max, big.NewInt(1))
+
+	n, err := rand.Int(rand.Reader, max)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return n, nil
 }
